b88b9daf0dfcf5aa56b5a297812b1f651c57da85
Wiktoria Demir <wiktoriaurszulademir@gmail.com>
2022-04-12 14:02:15 +0200

    "First JavaGame commit"

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+
+import java.awt.*;
+import jade.Window;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Window window = Window.get();
+        window.run();
+    }
+
+
+}
